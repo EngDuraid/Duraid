2499d49f1bada53ed443f21f904120323764d352
Eng.Duraid <Eng.Duraid@outlook.com>
2020-10-30 16:39:35 +0300

    Update the datacontext file

---
+++ Source/Duraid.Persistence/DuraidDataContext.cs
@@ -0,0 +1,68 @@
+﻿using Duraid.BusinessLogic.Common.Interfaces;
+using Duraid.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Duraid.Persistence
+{
+    public class DuraidDataContext :DbContext, IDataContext
+    {
+        public DuraidDataContext(DbContextOptions<DuraidDataContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           optionsBuilder.UseSqlite(connectionString: @"Data Source=./PingMonitorDb.db");
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ////modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+            //modelBuilder.Entity<Test>()
+            //.HasOne(p => p.Ip)
+            //.WithMany(b => b.Tests)
+            //.OnDelete(DeleteBehavior.Cascade);
+
+            //modelBuilder.Entity<State>()
+            //.HasOne(p => p.Test)
+            //.WithMany(b => b.States)
+            //.OnDelete(DeleteBehavior.Cascade);
+
+            //modelBuilder.Entity<Notification>()
+            //.HasOne(p => p.Test)
+            //.WithMany(b => b.Notifications)
+            //.OnDelete(DeleteBehavior.Cascade);
+
+
+            //modelBuilder.SeedData();
+        }
+
+        public override DatabaseFacade Database => base.Database;
+        public DbSet<Category> Categories {get;set;}
+        public DbSet<Post> Posts {get;set;}
+        public DbSet<Comment> Comments {get;set;}
+
+        public override void Dispose()
+        {
+            base.Dispose();
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken =default)
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        public override DbSet<TEntity> Set<TEntity>() where TEntity : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/Duraid.Persistence/InMemoryDataContext.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace Duraid.Persistence
+{
+    //public class InMemoryDataContext : DbContext, IDataContext
+    //{
+    //    public DbSet<Client> Clients { get; set; }
+    //    public DbSet<State> States { get; set; }
+    //    public DbSet<Test> Tests { get; set; }
+    //    public DbSet<Ip> Ips { get; set; }
+    //    public DbSet<Notification> Notifications { get; set; }
+
+
+
+
+    //}
+
+    public class InMemoryDataContext : DesignTimeDbContextFactoryBase<DuraidDataContext>
+    {
+        protected override DuraidDataContext CreateNewInstance(DbContextOptions<DuraidDataContext> options)
+        {
+            return new DuraidDataContext(options);
+        }
+    }
+    /// <summary>
+    /// These packages added to create this abstract class
+    /// </summary>
+    /// <typeparam name="TContext"></typeparam>
+
+
+    //Adding using Microsoft.Extensions.Configuration;
+    //Microsoft.Extensions.Configuration.Json;
+    //Microsoft.Extensions.Configuration.EnvironmentVariables;
+    //Microsoft.Extensions.Configuration.FileExtention;
+    //Microsoft.EntityFrameworkCore.SqlServer;
+
+    public abstract class DesignTimeDbContextFactoryBase<TContext> :
+        IDesignTimeDbContextFactory<TContext> where TContext : DbContext
+    {
+        private const string ConnectionStringName = "PingMonitorDatabase";
+        private const string AspNetCoreEnvironment = "ASPNETCORE_ENVIRONMENT";
+
+        public TContext CreateDbContext(string[] args)
+        {
+            var basePath = Directory.GetCurrentDirectory() + string.Format("{0}..{0}PingMonitorUI", Path.DirectorySeparatorChar);
+            return Create(basePath, Environment.GetEnvironmentVariable(AspNetCoreEnvironment));
+        }
+
+        protected abstract TContext CreateNewInstance(DbContextOptions<TContext> options);
+
+        private TContext Create(string basePath, string environmentName)
+        {
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.Local.json", optional: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var connectionString = configuration.GetConnectionString(ConnectionStringName);
+
+            return Create(connectionString);
+        }
+
+        private TContext Create(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentException($"Connection string '{ConnectionStringName}' is null or empty.", nameof(connectionString));
+            }
+
+            Console.WriteLine($"DesignTimeDbContextFactoryBase.Create(string): Connection string: '{connectionString}'.");
+
+            var optionsBuilder = new DbContextOptionsBuilder<TContext>();
+
+            optionsBuilder.UseSqlite(connectionString);
+            return CreateNewInstance(optionsBuilder.Options);
+        }
+    }
+}
